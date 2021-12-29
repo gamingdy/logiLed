4171d449f52761729d5a4d3a9c3180a6a2b8ea1e
Gamingdy <christgik12@gmail.com>
2021-12-29 16:07:02 +0100

    some change
    add dll file and add SetLightingForTargetZone function

---
+++ samples/test.py
@@ -0,0 +1,26 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+
+from logipy import logi_led
+import time
+import ctypes
+import random
+
+
+print("Initialize...")
+logi_led.logi_led_init()
+logi_led.logi_led_pulse_lighting(100, 100, 100, 3000, 500)
+time.sleep(3)
+logi_led.logi_led_set_lighting_for_target_zone(0, 0, 100, 0)
+for i in "a" * 100:
+    time.sleep(0.1)
+    zone = random.randint(1, 5)
+    red = random.randint(0, 100)
+    green = random.randint(0, 100)
+    blue = random.randint(0, 100)
+    logi_led.logi_led_set_lighting_for_target_zone(zone, red, green, blue)
+input("Press enter to shutdown SDK...")
+logi_led.logi_led_shutdown()
