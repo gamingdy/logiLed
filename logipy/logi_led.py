a5a9797e9f1fca9c9a56fe537a29027db39ceb3b
Gamingdy <christgik12@gmail.com>
2022-01-08 18:00:46 +0100

    code: move dll definition on top of file

--- logipy/logi_led.py
+++ logipy/logi_led.py
@@ -17,76 +17,6 @@ import platform
 import struct
 from pathlib import Path
 
-# Helpers
-#
-class Color:
-    """an RGBA color object that can be created using RGB, RGBA, color name, or a hex_code."""
-
-    def __init__(self, *args, **kwargs):
-        red, green, blue, alpha = 0, 0, 0, 255
-        hex_code = None
-        if len(args) > 0:
-            if isinstance(args[0], int):
-                red, green, blue = args[0], args[1], args[2]
-                if len(args) > 3:
-                    alpha = args[3]
-            elif isinstance(args[0], str):
-                if len(args) > 1:
-                    alpha = args[1]
-                if args[0] == "red":
-                    red, green, blue = 255, 0, 0
-                elif args[0] == "orange":
-                    red, green, blue = 255, 165, 0
-                elif args[0] == "yellow":
-                    red, green, blue = 255, 255, 0
-                elif args[0] == "green":
-                    red, green, blue = 0, 255, 0
-                elif args[0] == "blue":
-                    red, green, blue = 0, 0, 255
-                elif args[0] == "indigo":
-                    red, green, blue = 75, 0, 130
-                elif args[0] == "violet":
-                    red, green, blue = 238, 130, 238
-                elif args[0] == "cyan":
-                    red, green, blue = 0, 220, 255
-                elif args[0] == "pink":
-                    red, green, blue = 255, 0, 255
-                elif args[0] == "purple":
-                    red, green, blue = 128, 0, 255
-                elif args[0] == "white":
-                    red, green, blue = 255, 255, 255
-                elif args[0] == "black":
-                    red, green, blue = 0, 0, 0
-                else:
-                    hex_code = args[0]
-                    hex_code = kwargs.pop("hex", hex_code)
-        if hex_code:
-            hex_code = hex_code.replace("#", "")
-            self.red, self.green, self.blue = struct.unpack(
-                "BBB", hex_code.decode("hex")
-            )
-            self.alpha = alpha
-        elif any(x in ["red", "blue", "green", "alpha"] for x in kwargs):
-            self.red = kwargs.pop("red", red)
-            self.green = kwargs.pop("green", green)
-            self.blue = kwargs.pop("blue", blue)
-            self.alpha = kwargs.pop("alpha", alpha)
-        else:
-            self.red = red
-            self.green = green
-            self.blue = blue
-            self.alpha = alpha
-        self.hex_code = "#{h}".format(
-            h=struct.pack("BBB", *(self.red, self.green, self.blue)).encode("hex")
-        )
-
-    def rgb_percent(self):
-        return (
-            int((self.red / 255.0) * 100),
-            int((self.green / 255.0) * 100),
-            int((self.blue / 255.0) * 100),
-        )
-
 
 # DLL Definitions
 #
@@ -232,6 +162,77 @@ LOGI_DEVICETYPE_ALL = (
 # Required Globals
 #
 _LOGI_SHARED_SDK_LED = ctypes.c_int(1)
+
+
+# Helpers
+#
+class Color:
+    """an RGBA color object that can be created using RGB, RGBA, color name, or a hex_code."""
+
+    def __init__(self, *args, **kwargs):
+        red, green, blue, alpha = 0, 0, 0, 255
+        hex_code = None
+        if len(args) > 0:
+            if isinstance(args[0], int):
+                red, green, blue = args[0], args[1], args[2]
+                if len(args) > 3:
+                    alpha = args[3]
+            elif isinstance(args[0], str):
+                if len(args) > 1:
+                    alpha = args[1]
+                if args[0] == "red":
+                    red, green, blue = 255, 0, 0
+                elif args[0] == "orange":
+                    red, green, blue = 255, 165, 0
+                elif args[0] == "yellow":
+                    red, green, blue = 255, 255, 0
+                elif args[0] == "green":
+                    red, green, blue = 0, 255, 0
+                elif args[0] == "blue":
+                    red, green, blue = 0, 0, 255
+                elif args[0] == "indigo":
+                    red, green, blue = 75, 0, 130
+                elif args[0] == "violet":
+                    red, green, blue = 238, 130, 238
+                elif args[0] == "cyan":
+                    red, green, blue = 0, 220, 255
+                elif args[0] == "pink":
+                    red, green, blue = 255, 0, 255
+                elif args[0] == "purple":
+                    red, green, blue = 128, 0, 255
+                elif args[0] == "white":
+                    red, green, blue = 255, 255, 255
+                elif args[0] == "black":
+                    red, green, blue = 0, 0, 0
+                else:
+                    hex_code = args[0]
+                    hex_code = kwargs.pop("hex", hex_code)
+        if hex_code:
+            hex_code = hex_code.replace("#", "")
+            self.red, self.green, self.blue = struct.unpack(
+                "BBB", hex_code.decode("hex")
+            )
+            self.alpha = alpha
+        elif any(x in ["red", "blue", "green", "alpha"] for x in kwargs):
+            self.red = kwargs.pop("red", red)
+            self.green = kwargs.pop("green", green)
+            self.blue = kwargs.pop("blue", blue)
+            self.alpha = kwargs.pop("alpha", alpha)
+        else:
+            self.red = red
+            self.green = green
+            self.blue = blue
+            self.alpha = alpha
+        self.hex_code = "#{h}".format(
+            h=struct.pack("BBB", *(self.red, self.green, self.blue)).encode("hex")
+        )
+
+    def rgb_percent(self):
+        return (
+            int((self.red / 255.0) * 100),
+            int((self.green / 255.0) * 100),
+            int((self.blue / 255.0) * 100),
+        )
 
 
 class SDKNotFoundException(BaseException):
