02e858b9d55cc6fae2d6d4cb72a2a3109ea2fdb3
Gamingdy <christgik12@gmail.com>
2022-08-06 18:42:07 +0200

    update test.py

--- samples/test.py
+++ samples/test.py
@@ -1,22 +1,19 @@
+import time
 import sys
 import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
+from logipy.logi_led import NotImplemented, LogitechLed, load_dll
 
-from logipy.logi_led import NotImplemented, LogitechLed
-import time
-import ctypes
-import random
-
-
+load_dll()
 print("Initialize...")
 logi_led = LogitechLed()
 b = logi_led.pulse_lighting(50, 100, 0, 0, 500)
 time.sleep(5)
 logi_led_test = NotImplemented()
 
-b = logi_led_test.stop_effects()
+b = logi_led.stop_effects()
 
 input("Press enter to shutdown SDK...")
 logi_led.shutdown()
