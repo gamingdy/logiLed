25e30d699ee7127e7719a6857e7971a3f6027b04
Gamingdy <christgik12@gmail.com>
2023-02-07 12:35:05 +0100

    rename the doc_generator folder to doc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,7 @@ import os
 sys.path.insert(0, os.path.abspath("../../logipy"))
 
 project = "logiPy"
-copyright = "2022, Gamingdy"
+copyright = "2022-2023, Gamingdy"
 author = "Gamingdy"
 
 # The full version, including alpha/beta/rc tags
@@ -34,7 +34,12 @@ release = "1.0"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.duration",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -49,7 +54,13 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "renku"
+html_theme = "furo"
+html_theme_options = {
+    "top_of_page_button": "edit",
+}
+
+
+html_last_updated_fmt = "%d/%b/%Y"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
