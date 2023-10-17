4d18acb984dd8dcb3bda2c88e709f868773f1fec
Gamingdy <christgik12@gmail.com>
2023-10-17 18:48:30 +0200

    feat:DOC:remove my logo from doc favicon

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -62,8 +62,8 @@ html_theme_options = {
 
 
 html_last_updated_fmt = "%d/%b/%Y"
-html_favicon = "https://gamingdy.me/img/favicon.png"
-html_logo = "https://gamingdy.me/img/favicon.png"
+html_favicon = ""
+html_logo = ""
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
