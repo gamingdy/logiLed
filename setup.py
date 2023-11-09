f900ccf791cf5b814e07388ffd8f1687a927e2b9
Gamingdy <christgik12@gmail.com>
2023-11-09 20:11:49 +0100

    feat: move package configuration in pyproject.toml

--- setup.py
+++ setup.py
@@ -1,26 +1,4 @@
 from setuptools import setup
 
-setup(
-    name="logiled",
-    version="2023.11.9",
-    author="Gamingdy",
-    description="A simple python wrapper for Logitech G's LED",
-    long_description=open("README.rst").read(),
-    keywords=[
-        "logi",
-        "logiled",
-        "Logitech",
-        "LogitechG",
-        "LED",
-        "LGS",
-        "Logitech Gaming Software",
-    ],
-    url="https://github.com/gamingdy/logiLed",
-    download_url="https://github.com/gamingdy/logiLed",
-    packages=["logiled"],
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Topic :: Software Development :: Libraries",
-    ],
-    license="MIT",
-)
+if __name__ == "__main__":
+    setup()
